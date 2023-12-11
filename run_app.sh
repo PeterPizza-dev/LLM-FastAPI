0a107c0a545f7388f4e5b8a10aad275003f21419
PeterPizza-dev <oliverbehrens@hotmail.com>
2023-12-11 08:59:55 +0100

    updated bash to look for backend before launching frontend

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,13 @@
-uvicorn backend.app:app --reload &
+uvicorn backend.app:app --log-config=log_conf.yaml &
+UVICORN_PID=$!
+
+# Wait for uvicorn to start (you can adjust the sleep duration as needed)
 sleep 5
-streamlit run frontend/LEGO_employee_chatbot.py
+
+# Check if uvicorn is still running
+if kill -0 $UVICORN_PID 2>/dev/null; then
+    echo "UVicorn started successfully, launching Streamlit"
+    streamlit run frontend/LEGO_employee_chatbot.py
+else
+    echo "UVicorn failed to start. Aborting."
+fi
